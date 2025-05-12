d974e8dbdce3b91b2b3e690e0636d2e0f9f515ea
Thomas <boy1.thoomas@gmail.com>
2025-05-12 16:51:54 +0200

    feat: add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
