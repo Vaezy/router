dbb44319be0e0746f64cb85a305b278301c4136a
Thomas <boy1.thoomas@gmail.com>
2025-05-14 10:08:41 +0200

    feat: add button to return to home page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,11 +1,20 @@
-import { useParams } from "react-router";
+import { Button } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router";
 
 export const Category = () => {
   const { category } = useParams();
+  const navigate = useNavigate();
+
+  const hReturn = () => {
+    navigate("/");
+  };
 
   return (
     <div>
       <h1>Catégorie : {category}</h1>
+      <Button variant="primary" onClick={hReturn}>
+        Retour à la page d'accueil
+      </Button>
     </div>
   );
 };
